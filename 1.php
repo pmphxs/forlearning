d664ef58a63ea5af7ba5f53ecbe4fa13ae9e7e80
soul11201 <1479418380@qq.com>
2014-07-09 22:17:54 +0800

    乱七八糟，，模拟http请求方法

--- 1.php
+++ 1.php
@@ -1,2 +1,8 @@
 <?php
 	echo 0=='0';
+
+	//测试一array_push的功能，居然直接把var当成一个整体压进去了，成为二维的了。。。。。不理想,用foreach +直接赋值还可以
+	$stack = array(19);
+	$var = array(1,2,3,4,5,6,7);
+	array_push($stack, $var);
+	var_dump($stack);
--- php_curl_request.php
+++ php_curl_request.php
@@ -1,5 +1,42 @@
 <?php
 
-	function request($url,$header){
+	function request($url,$method='get',$post_data='',$header='',$setop=array()){
+		//post 所有的配置需要$setop传过来
+		//1
+		$handle = curl_init($url);
 
+		//2
+		//设置options
+		$options = array(
+	        //请求相关设置			
+			//数据流形式返回，不返还给标准输出stdout浏览器
+			CURLOPT_RETURNTRANSFER=>1,
+
+			//自定义头信息
+			CURLOPT_HTTPHEADER=>$header,
+			
+			//重定向相关
+			CURLOPT_FOLLOWLOCATION=>1,
+			CURLOPT_AUTOREFFER=>1,
+			CURLOPR_MAXREDIRS=>3,
+			
+			//等待时间
+			CURLOPT_CONNECTTIMEOUT=>3
+
+		);
+		foreach ($setop as $key => $value) {
+			$options[$key] = $value;
+		}
+		curl_setopt_array($handle,$options);
+		
+		//3
+		$response_data_stream = curl_exec($handle);
+		if($response_data_stream === false){
+			echo curl_getinfo($handle);
+		}
+		//4
+		curl_close($handle);
+		return $response_data_stream;
 	}
+	$data = stream_get_contents(STDIN);
+	echo $data;

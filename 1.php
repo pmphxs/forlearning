1d03a572c74fdeb2329ca39a588f63f802ef97d3
soul11201 <1479418380@qq.com>
2014-07-10 10:45:45 +0800

    修改http请求方法bug 脚本从命令行运行获取参数。。

--- 1.php
+++ 1.php
@@ -2,7 +2,14 @@
 	echo 0=='0';
 
 	//测试一array_push的功能，居然直接把var当成一个整体压进去了，成为二维的了。。。。。不理想,用foreach +直接赋值还可以
-	$stack = array(19);
-	$var = array(1,2,3,4,5,6,7);
-	array_push($stack, $var);
-	var_dump($stack);
+	// $stack = array(19);
+	// $var = array(1,2,3,4,5,6,7);
+	// array_push($stack, $var);
+	// var_dump($stack);
+
+	//php 命令行输入参数
+	print_r($argv);
+	echo '\r\n';
+	print_r($argc);
+
+
--- php_curl_request.php
+++ php_curl_request.php
@@ -1,6 +1,12 @@
 <?php
+	//老是报 警告和一堆如下类似的提示，暂时未发现很好的解决方法，有博客说 CURLOPT_RETURNTRANSFER写成 ‘CURLOPT_RETURNTRANSFER’根本就是错
+    //后来发下提示错误是因为常量拼写错误
+	// PHP Warning:  curl_setopt_array(): Array keys must be CURLOPT constants or equivalent integer values
+	// Use of undefined constant CURLOPR_MAXREDIRS - assumed 'CURLOPR_MAXREDIRS'
+	//error_reporting(E_ALL & ~E_NOTICE);
 
-	function request($url,$method='get',$post_data='',$header='',$setop=array()){
+
+	function def_request($url,$method='get',$post_data='',$header=array(),$setop=array()){
 		//post 所有的配置需要$setop传过来
 		//1
 		$handle = curl_init($url);
@@ -17,26 +23,43 @@
 			
 			//重定向相关
 			CURLOPT_FOLLOWLOCATION=>1,
-			CURLOPT_AUTOREFFER=>1,
-			CURLOPR_MAXREDIRS=>3,
+			CURLOPT_AUTOREFERER=>1,
+			CURLOPT_MAXREDIRS=>3,
 			
 			//等待时间
-			CURLOPT_CONNECTTIMEOUT=>3
-
+			CURLOPT_CONNECTTIMEOUT=>3,
+			//响应相关的设置
+			//c
+			//CURLOPT_HTTPHEADER=>0,
 		);
+
 		foreach ($setop as $key => $value) {
 			$options[$key] = $value;
 		}
+
+		//自定义头信息----- 没有用的。。。。居然想着让 $header 传字符串***..
+		// if(empty($header)){
+		// 	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+		// }
+
+		var_dump($options);
 		curl_setopt_array($handle,$options);
 		
 		//3
 		$response_data_stream = curl_exec($handle);
 		if($response_data_stream === false){
-			echo curl_getinfo($handle);
+			echo curl_errno($handle).'  '.curl_error($handle);
+			//curl_getinfo($handle);
 		}
 		//4
 		curl_close($handle);
 		return $response_data_stream;
 	}
-	$data = stream_get_contents(STDIN);
+
+
+	//$data = stream_get_contents(STDIN);
+
+	$input = getopt('u:');
+	//var_dump($input);
+	$data = def_request($input['u']);
 	echo $data;

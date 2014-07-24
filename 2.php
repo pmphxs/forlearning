24286242ade5b711c70bd5f42dd693897f0eeac5
杨振 <1479418380@qq.com>
2014-07-24 15:01:21 +0800

    函数引用 构建http参数

--- 2.php
+++ 2.php
@@ -1,11 +1,32 @@
 <?php
 
-
-function fadfa($d){
-
-	echo 1;
-	echo $d;
-
+function aaaa(&$a='dfafaf'){
+	echo $a;
+	$a ='ddddddd';
 }
-$a='fadfa';
-$a(3333333333);
+aaaa();
+echo "\n";
+$b  = 'aaaaaaaaaa';
+aaaa($b);
+
+aaaa();
+
+aaaa($b);
+
+// $a = array
+// (
+//     'sec_id' => 'MD5',
+//     'v' => '1.0',
+//   );
+// echo http_build_query($a);
+
+
+
+// function fadfa($d){
+
+// 	echo 1;
+// 	echo $d;
+
+// }
+// $a='fadfa';
+// $a(3333333333);

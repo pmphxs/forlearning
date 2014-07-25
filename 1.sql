e48b6b23b04cdd65f729d867c6951064e3cb5e07
杨振 <1479418380@qq.com>
2014-07-25 21:51:21 +0800

    没事瞎写个数据库 操作系统的学习笔记，看视频，看书。

---
+++ 1.sql
@@ -0,0 +1,17 @@
+create table if not exists consume(
+	id int primary key not null auto_increment unsigned,
+	user_name varchar(17) not null default '', 
+	createat datatime not null default '2014-07-25 21:33:59',
+	money float not null default '0.0',
+	desc varchar(255) not null default ''
+);
+create table if not exists work(
+	id int not null primary key auto_increment unsigned,
+	user_name varchar(17) not null default '',
+	createat datatime not null default '2014-07-25 21:41:59',
+	finishat datatime not null default '2014-07-25 21:41:59',
+	task_abstract varchar(200) not null default '',
+	status ENUM(0,1,2) not null default '0' 
+);
+
+

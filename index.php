93d82eee476f0efd9f16bcb68c7052d0c73e42cd
unknown <1479418380@qq.com>
2014-08-18 00:40:28 +0800

    简化函数，实现相同的功能,is_dir函数还是没有使用正确无法递归调用

--- index.php
+++ index.php
@@ -4,18 +4,18 @@
 	//$root = scandir($cwd);
 	//var_dump($root);
 	var_dump(is_dir(".\img"));
-	traverse_all_dir_convert2_url('.');
+	traverse_all_dir('.\img');
 
-	function traverse_all_dir_convert2_url($begin,$url='.'){
+	function traverse_all_dir($begin='.'){
 		$all  = opendir($begin);
 
 		while(false !== ($file =readdir($all))){
-			echo $path = $url.DIRECTORY_SEPARATOR.$file."\n";
+			echo $path = $begin.DIRECTORY_SEPARATOR.$file."\n";
 			
 			//echo is_dir($path)?"1\n":"0\n";
 			
 			if($path !== '.' && $path !== '..' && is_dir($path)){
-				traverse_all_dir_convert2_url($path);
+				traverse_all_dir($path);
 			}
 		}
 

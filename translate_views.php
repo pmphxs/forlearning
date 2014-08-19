32e1844eb99858a34947115434e7ac0500b9a269
soul11201 <1479418380@qq.com>
2014-08-19 16:12:24 +0800

    modify translate_views

--- translate_views.php
+++ translate_views.php
@@ -4,7 +4,7 @@
 <?php
 	$language_arr = array('zh','en');
 	for ($i=0; $i < 10 ; $i++) { 
-		echo '<input type= "text" name = "source_text"/><input type="text" name="source_language"><input type="text" name="target_language">'."\n";
+		echo '<p> 文本 <input type= "text" name = "source_text"/> 源语言 <input type="text" name="source_language">目标语言<input type="text" name="target_language"><p>';
 		// foreach ($language_arr as $value) {
 		// 	echo '<select name="">'
 		// }

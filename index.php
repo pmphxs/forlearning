577ca1c8057b2d3c2f97978dbb6aa3fcdba023f2
unknown <1479418380@qq.com>
2014-08-25 01:49:25 +0800

    目录的g遍历

--- index.php
+++ index.php
@@ -1,27 +1,53 @@
 <?php
-
 	//$cwd = getcwd();
 	//$root = scandir($cwd);
 	//var_dump($root);
 	var_dump(is_dir(".\img"));
-	traverse_all_dir('D:\2014\github\forlearning');
-
+	$res = traverse_all_dir('D:\2014\github\forlearning');
+	//echo file_exists('.\img');
+    //var_dump(is_file(''));
+	// traverse_all_dir('.');
+	//$count =0;
 	function traverse_all_dir($begin='.'){
 		
+		global $count;
 		
-		if(is_dir($begin)){
-			$all  = opendir($begin);
-		
-			while(false !== ($file =readdir($all))){	
-	
-				echo $path = $begin.DIRECTORY_SEPARATOR.$file;
-				echo "\n";
-				if($path !== '.' && $path !== '..' && $path !== '.git'){
+		if($all  = opendir($begin)){
+			
+			$res = array();
+			// $all  = opendir($begin);
+			
+			while(false !== ($file =readdir($all))){		
+				
+				$path = $begin.DIRECTORY_SEPARATOR.$file;
+				// echo $path."\n";
+				// if(is_file($path) && $path){
+				// 	//echo $path;
+				// 	$res[] = $path;
+				// 	//continue;
+				// }
 
-					traverse_all_dir($path);
+				if($file !== '.' && $file !== '..' && $file !== '.git'){
+					//array_push(, var)
+					// array_push($res,traverse_all_dir($path));
+					echo $path."\n";
+					if(is_dir($path)){
+						// traverse_all_dir($path);	
+						array_push($res,traverse_all_dir($path));
+					}else{
+						$res[] = $path;
+						$count +=1;
+					}
+
 				}
 			}
 			closedir($all);	
+			return $res;
 		}
+		// }else if(file_exists($begin)){
+		// }
 	}
+
+ var_dump($res);
+ echo $count;
 

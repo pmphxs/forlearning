f560cc984e3a75cf509f012b3ee90043cfbdf40f
unknown <1479418380@qq.com>
2014-08-25 00:55:36 +0800

    modify

--- index.php
+++ index.php
@@ -4,21 +4,24 @@
 	//$root = scandir($cwd);
 	//var_dump($root);
 	var_dump(is_dir(".\img"));
-	traverse_all_dir('.\img');
+	traverse_all_dir('D:\2014\github\forlearning');
 
 	function traverse_all_dir($begin='.'){
-		$all  = opendir($begin);
+		
+		
+		if(is_dir($begin)){
+			$all  = opendir($begin);
+		
+			while(false !== ($file =readdir($all))){	
+	
+				echo $path = $begin.DIRECTORY_SEPARATOR.$file;
+				echo "\n";
+				if($path !== '.' && $path !== '..' && $path !== '.git'){
 
-		while(false !== ($file =readdir($all))){
-			echo $path = $begin.DIRECTORY_SEPARATOR.$file."\n";
-			
-			//echo is_dir($path)?"1\n":"0\n";
-			
-			if($path !== '.' && $path !== '..' && is_dir($path)){
-				traverse_all_dir($path);
+					traverse_all_dir($path);
+				}
 			}
+			closedir($all);	
 		}
-
-		closedir($all);	
 	}
 
